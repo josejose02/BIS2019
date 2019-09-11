69b2574c71eee0086e97e6659377c41fc1e8e818
José David Vázquez <josedvzsg@gmail.com>
2019-09-11 19:07:47 +0200

    Added comments

--- homework/Homework1.py
+++ homework/Homework1.py
@@ -1,15 +1,20 @@
 import math
+
+# Task One
 print("Hello {}!".format(input("Input your name, please: ")))
 
+# Task Two
 print("\nEnter two numbers.")
 number_one = float(input("Number one: "))
 number_two = float(input("Number two: "))
 division = number_one / number_two
 print("The rounded up division is: {}".format(math.ceil(division)))
 
+# Task Three
 radius = float(input("\nPlease enter a radius: "))
 print("The area is {:.2f} m2".format(math.pi * radius ** 2))
 
+# Extra Task
 print("\nCalculator 2: Electric Boogaloo")
 operation = input(">> ")
 print(eval(operation))
