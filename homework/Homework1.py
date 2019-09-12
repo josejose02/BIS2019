c8184cfcfd8a2764f031395e6b1900294df05ae9
José David Vázquez <josedvzsg@gmail.com>
2019-09-12 10:06:52 +0200

    Small structure change

--- homework/Homework1.py
+++ homework/Homework1.py
@@ -7,8 +7,8 @@ print("Hello {}!".format(input("Input your name, please: ")))
 print("\nEnter two numbers.")
 number_one = float(input("Number one: "))
 number_two = float(input("Number two: "))
-division = number_one / number_two
-print("The rounded up division is: {}".format(math.ceil(division)))
+division = math.ceil(number_one / number_two)
+print("The rounded up division is: {}".format(division))
 
 # Task Three
 radius = float(input("\nPlease enter a radius: "))
